a59fbf383be10bf864baef888336680272c9bba8
tbduong <tiffaniduong@gmail.com>
2022-05-30 21:58:32 -0700

    Add basic task appending and basic css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,47 @@
 $(document).ready(function(){
     console.log("Hello World");
+
+    var todoList = $('.todo-list');
+    var todoInput = $('.todo-input');
+    var notification = $('#notification');
+
+//attach event handler to the add button
+$('#todo-add').on("click", function(e){
+    e.preventDefault();
+
+    //return false if input is empty
+    if(!todoInput.val()) { 
+        console.log("no input");
+        return false; 
+    } else {
+    //add new todo item to todo list
+        todoList.append("<li>" + todoInput.val());
+        notification.css("display", "none");
+
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
